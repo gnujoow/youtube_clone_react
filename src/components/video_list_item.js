036f13ba363c8b91fc82f5d124c93d7334890413
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-05 20:03:11 +0900

    markup video_list_item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   // {video}는 const video = props.video 와 같다.
+  const imgUrl = video.snippet.thumbnails.default.url;
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imgUrl} />
         </div>
-
         <div className="media-body">
-          <div className="media-heading"></div>
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
         </div>
       </div>
     </li>
